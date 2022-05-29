fb59a39c15a7edd96568b110a64d91cee776f0d6
Praveen Dias <praveendias1180@gmail.com>
2022-05-29 10:37:24 +0530

    optional property/elecment access and call operator

--- src/chaining.ts
+++ src/chaining.ts
@@ -6,6 +6,13 @@ function getCustomer(id: number): Customer | null {
     return id === 0 ? null : {birthday: new Date()}
 }
 
-let customer = getCustomer(1)
+let customer = getCustomer(0)
 // optional property access operator
-console.log(customer?.birthday)
+console.log(customer?.birthday.getFullYear())
+
+// optional element access operator
+// customers?.[0]
+
+// optional call operator
+// let log: any = null
+// log?.('a')
